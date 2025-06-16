0f6682c6fe12759c0d9de5c04ebeaae8c68c61e7
Dimitrios Lowe <dimitrioslowe@gmail.com>
2025-06-16 10:03:58 -0400

    submission

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,24 @@
+const BASE_API = "https://fitnesstrac-kr.herokuapp.com/api/activities";
+import { useQuery } from "@tanstack/react-query";
+
 export default function ActivitiesPage() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const res = await fetch(BASE_API);
+      const activities = await res.json();
+      return activities;
+    },
+  });
   return (
     <>
       <h1>Activities</h1>
       <p>Imagine all the activities!</p>
+      <ul>
+        {data?.map((activity) => (
+          <li key={activity.id}>{activity.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "./layout/Layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { AuthProvider } from "./auth/AuthContext";
 import { ApiProvider } from "./api/ApiContext";
 import { PageProvider } from "./layout/PageContext";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <ApiProvider>
-      <PageProvider>
-        <Layout>
-          <App />
-        </Layout>
-      </PageProvider>
-    </ApiProvider>
-  </AuthProvider>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <ApiProvider>
+        <PageProvider>
+          <Layout>
+            <App />
+          </Layout>
+        </PageProvider>
+      </ApiProvider>
+    </AuthProvider>
+  </QueryClientProvider>
 );
